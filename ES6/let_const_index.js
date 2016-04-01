99983e7b9a6187356a7400c992d5e5fc8b223e71
疯哥 <emacs@vimer.org>
2016-04-01 13:59:52 +0800

    fix

--- ES6/let_const_index.js
+++ ES6/let_const_index.js
@@ -283,11 +283,13 @@
   *window.a // 2
   */
 
-var a = 1;
- //如果在Node的REPL环境，可以写成global.a
- // 或者采用通用方法，写成this.a
-window.a // 1
-let b = 1;
-window.b // undefined
+/*
+ *var a = 1;
+ * //如果在Node的REPL环境，可以写成global.a
+ * // 或者采用通用方法，写成this.a
+ *window.a // 1
+ *let b = 1;
+ *window.b // undefined
+ */
 
 
--- Web/angular-gulp-jade-less-sass-node/gulpfile.js
+++ Web/angular-gulp-jade-less-sass-node/gulpfile.js
@@ -1,13 +1,36 @@
 var path = require("path");
 var gulp = require('gulp');
 var	$  = require('gulp-load-plugins')();
+var gulpsync    = $.sync(gulp);
 
-gulp.task('default',  function(){
+var paths =  {
+	app : "../app/"
+}
+
+var build = {
+	scripts: paths.app + "js",
+	scripts: paths.app + "css"
+}
+
+gulp.task('vendor',  gulpsync.sync(['vendor:base', 'vendor:app']));
+
+gulp.task('vendor:base', function() {
+	//return gulp.src(vendor.base.source)
+
+});
+
+gulp.task('vendor:app', function() {
+
+});
+
+gulp.task('default', gulpsync.sync([
+								   'vendor'
+]), function(){
 	log('************');
 	log('* All Done * You can start editing your code, LiveReload will update your browser after any change..');
 	log('************');
 });
 
 function log(msg) {
-	//$.util.log($.util.colors.blue(msg));
+	$.util.log($.util.colors.blue(msg));
 }

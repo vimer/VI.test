85c9f4863691e6bf60b0eab5bec847e0d7599f83
疯哥 <emacs@vimer.org>
2016-06-06 15:46:05 +0800

    fix

--- Web/ionic/component/modal/script.js
+++ Web/ionic/component/modal/script.js
@@ -1,57 +1,50 @@
 angular.module('ionicApp', ['ionic'])
 
 .controller('AppCtrl', function ($scope, $ionicModal) {
+	console.log($scope);
+	$scope.contacts = [
+	{
+		name: 'Gordon Freeman'
+	},
+	{
+		name: 'Barney Calhoun'
+	},
+	{
+		name: 'Lamarr the Headcrab'
+	}
+	];
 
-  console.log($scope);
-
-  $scope.contacts = [
-    {
-      name: 'Gordon Freeman'
-    },
-    {
-      name: 'Barney Calhoun'
-    },
-    {
-      name: 'Lamarr the Headcrab'
-    }
-  ];
-
-  $ionicModal.fromTemplateUrl('modal.html', function (modal) {
-    $scope.modal = modal;
-  }, {
-    animation: 'slide-in-up',
-    focusFirstInput: true
-  });
+	$ionicModal.fromTemplateUrl('modal.html', function (modal) {
+		$scope.modal = modal;
+	}, {
+		animation: 'slide-in-up',
+		focusFirstInput: true
+	});
 
 })
-
 .controller('ModalCtrl', function ($scope) {
-
-  $scope.newUser = {};
-
-  $scope.createContact = function () {
-    console.log('Create Contact', $scope.newUser);
-    $scope.modal.hide();
-  };
+	$scope.newUser = {};
+	$scope.createContact = function () {
+		console.log('Create Contact', $scope.newUser);
+		$scope.modal.hide();
+	};
 
 })
 .controller('SecondCtrl', function ($scope,$ionicModal) {
 
-        console.log($scope);
-        $ionicModal.fromTemplateUrl('modal2.html', function (modal) {
-            $scope.modal = modal;
-        }, {
-            animation: 'slide-in-up',
-            focusFirstInput: true
-        });
- })
+	console.log($scope);
+	$ionicModal.fromTemplateUrl('modal2.html', function (modal) {
+		$scope.modal = modal;
+	}, {
+		animation: 'slide-in-up',
+		focusFirstInput: true
+	});
+})
 .controller('Modal2Ctrl', function ($scope) {
-
-    $scope.newUser = {};
-
-    $scope.createContact = function () {
-        console.log('Create Contact', $scope.newUser);
-        $scope.modal.hide();
-    };
+	$scope.newUser = {};
+	$scope.createContact = function () {
+		console.log('Create Contact', $scope.newUser);
+		$scope.modal.hide();
+	};
 
 });

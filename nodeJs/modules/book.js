2e0bdd40a3526c4bfd1339b3764847b1a6609949
疯哥 <emacs@vimer.org>
2016-08-29 18:13:41 +0800

    fix

--- nodeJs/modules/book.js
+++ nodeJs/modules/book.js
@@ -9,4 +9,7 @@ Book.prototype.show = function() {
 module.exports = Book
 //exports = Book //exports只是module.exports的一个快捷方式，改变exports的值并不能改变module.exports的值
 
-console.log(module.parent)
+console.log(module.parent) //可以作为入口的判断
+if (!module.parent) {
+	//.....
+}

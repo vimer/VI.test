c23d73c5ce07b9129128483f60e6b92919c67583
疯哥 <emacs@vimer.org>
2016-11-13 14:15:54 +0800

    fix

---
+++ nodeJs/ES6/promise.js
@@ -0,0 +1,57 @@
+/*
+ *Promise.resolve(1).then(x => {
+ *    console.log(x);
+ *    return x+1
+ *}).then(y => {
+ *    console.log(y);
+ *    return Promise.resolve(y+1);
+ *}).then(z => {
+ *    console.log(z);
+ *    return Promise.reject('wrong');
+ *}).then(n => {
+ *    console.log(n);
+ *})
+ *.catch(e => {
+ *    console.log(e)
+ *})
+ */
+
+
+//callback和Promise的转化
+/*
+ *const promisefy = (nodefn) => (...args) => 
+ *new Promise((resolve, reject) => {
+ *    nodefn(...args, function(err, res) {
+ *        if (err) {
+ *            return reject(err)
+ *        }
+ *        resolve(res)
+ *    })
+ *})
+ *const fs = require('fs');
+ *const readFile = promisefy(fs.readFile);
+ *readFile("./async.js", 'utf8').then(content => {
+ *    return 'hello' + content;
+ *}).then(helloContent => {
+ *    console.log(helloContent)
+ *}).catch(err => {
+ *    console.log(err.message)
+ *})
+ */
+
+/*
+ *var fetch = require('node-fetch')
+ *fetch('http://baidu.com')
+ *.then(res => res.text())
+ *.then(text => {
+ *    console.log(text);
+ *}).catch(err => {
+ *    console.log(err);
+ *})
+ */
+
+//Promise.all
+Promise.all([Promise.resolve(1), Promise.resolve(2)])
+.then(results => console.log(results))
+
+
